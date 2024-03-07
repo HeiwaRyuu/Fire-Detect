fbe856b381a8d39458645651f0eaf09d27cf917d
Vitor Lima <vitor.carvalho.ufu@gmail.com>
2024-03-07 00:04:36 -0300

    First Acceptable Version - Detection Still Misses

---
+++ fire_detect.py
@@ -0,0 +1,123 @@
+import cv2
+import numpy as np
+from mss import mss
+from ultralytics import YOLO
+import time
+
+def get_center_screen_coordinates(width=608, height=608):
+    with mss() as sct:
+        monitor = sct.monitors[1]
+        ## CENTER
+        center_left = (monitor["width"] - width) // 2
+        center_top = (monitor["height"] - height) // 2
+
+        return center_left, center_top, width, height
+
+def letterbox(image, expected_size):
+    """Resize image to expected size, maintaining aspect ratio with padding."""
+    ih, iw = image.shape[:2]
+    eh, ew = expected_size, expected_size
+
+    # SCALE
+    scale = min(ew/iw, eh/ih)
+    nw, nh = int(iw * scale), int(ih * scale)
+    image_resized = cv2.resize(image, (nw, nh))
+
+    # PADD
+    # top = 20 #(eh - nh) // 2
+    # bottom = 0 #eh - nh - top
+    # left = 0 #(ew - nw) // 2
+    # right = 0 #ew - nw - left
+
+    # APP PADD
+    # image_padded = cv2.copyMakeBorder(image_resized, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(128, 128, 128))
+    
+    return image_resized
+
+
+def draw_bounding_box(frame, box, class_name, confidence):
+    # COORDS
+    x1, y1, x2, y2 = [int(coord) for coord in box]
+
+    # LABEL BG
+    label = f"{class_name} {confidence:.2f}"
+    (text_width, text_height), baseline  = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
+    text_start_x = x1
+    if y1>text_height+baseline:
+        text_start_y = y1-text_height
+    else:
+        text_start_y = y1+text_height
+    
+    # BOUNDING BOX RECT
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    # LABEL RECT
+    cv2.rectangle(frame, (text_start_x, text_start_y-text_height), (text_start_x + text_width + baseline, text_start_y + text_height), (0, 0, 255), cv2.FILLED)
+    # LABEL
+    cv2.putText(frame, label, (text_start_x+baseline, text_start_y+baseline), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+
+def detect_fire_and_smoke(model, frame):
+    results = model.predict(frame, verbose=False)
+    fire_detected = False
+    smoke_detected = False
+    result = results[0]
+    confidence_threshold = 0.20
+
+    for i in range(len(result.boxes.data)):
+        cls_id = int(result.boxes.cls[i].item())
+        conf = result.boxes.conf[i].item()
+        if conf > confidence_threshold:
+            bbox = result.boxes.xyxy[i].tolist()  # [x1, y1, x2, y2]
+            class_name = result.names[cls_id].lower()
+            draw_bounding_box(frame, bbox, class_name, conf)
+            if class_name == 'fire':
+                fire_detected = True
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 0, 255), 2)
+                # break
+            elif class_name == 'smoke':
+                smoke_detected = True
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), 2)
+                # break
+
+    return fire_detected, smoke_detected
+
+
+def main():
+    model_path="FIRE.v1i.yolov8-obb/YOLOv8-Fire-and-Smoke-Detection/runs/detect/train/weights/best.pt"
+    model = YOLO(model_path)
+
+    model_input_size = 608 
+    left, top, width, height = get_center_screen_coordinates(width=model_input_size, height=model_input_size)
+    
+    # CAPTURE REGION
+    mon = {
+        "top": top,
+        "left": left,
+        "width": width,
+        "height": height
+    }
+
+    with mss() as sct:
+        try:
+            while True:
+                # SCREEN CAPTURE
+                sct_img = sct.grab(mon)
+                frame = np.array(sct_img)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR) ## CONVERTING TO RGB
+                # frame = letterbox(frame, model_input_size) ## ONLY IF I CHANGE CAPTURE REGION
+
+                is_fire, is_smoke = detect_fire_and_smoke(model, frame)
+                if is_fire:
+                    print("Fire detected!")
+                if is_smoke:
+                    print("Smoke detected!")
+
+                cv2.imshow('Screen', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)) ## DISPLAY
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        finally:
+            cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
